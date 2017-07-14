91fbbfc7a1853da3f45fef5d8089f927df8d43ed
prakash <pgk639117@gmail.com>
2017-07-14 14:04:10 +0200

    chat with nodejs

---
+++ server.js
@@ -0,0 +1,50 @@
+var express=require('express');
+ var   app= express();
+var   server=require('http').createServer(app);
+ var   io=require('socket.io').listen(server);
+usernames=[];
+
+server.listen(process.env.PORT || 3000);
+console.log('server running');
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+
+});
+
+io.sockets.on('connection', function(socket){
+    // connections.push(socket);
+    console.log("socket connected");
+socket.on('new user', function(data,callback){
+    if(usernames.indexOf(data) != -1){
+        callback(false);
+
+    }else{
+        callback(true);
+        socket.username=data;
+        usernames.push(socket.username);
+        updateUsernames();
+    }
+});
+
+    //update usernames
+
+    function updateUsernames(){
+        io.sockets.emit('usernames',usernames);
+    }
+//send Msg
+socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg:data,user:socket.username});
+
+});
+// Disconnect
+    socket.on('disconnect', function(data){
+        if(!socket.username){
+            return;
+        }
+        usernames.splice(usernames.indexOf(socket.username),1);
+        updateUsernames();
+    });
+});
+
+
